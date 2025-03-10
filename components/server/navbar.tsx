e293633f23e4cee9527e1f6bb333a86ed9ec65ff
Johan Kremer <johan.h.kremer@gmail.com>
2025-03-10 15:06:40 +0100

    Add lucide-react icon support. Add navbar with Radix avatar with functional link to main page and section links, not yet functional.

--- components/server/navbar.tsx
+++ components/server/navbar.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import { Avatar, Flex } from '@radix-ui/themes'
+import Link from 'next/link'
 
 export default function Navbar() {
     return (
-        <nav>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
+        <nav className='flex flex-row py-4 justify-between items-center'>
+            <Flex align="center" gap="4">
+                <Link href="/">
+                    <Avatar
+                        size="5"
+                        src="/profile.png"
+                        fallback="A"
+                        radius='full'
+                    />
+                </Link>
+            </Flex>
+            <ul className='flex flex-row items-center gap-8 text-primary-200'>
+                <li>
+                    <h6>Projects</h6>
+                </li>
+                <li>
+                    <h6>About</h6>
+                </li>
+                <li>
+                    <h6>Contact</h6>
+                </li>
             </ul>
         </nav>
     )
