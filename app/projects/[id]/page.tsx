e793551be3726188a92721634476f6a0b57c3197
Johan Kremer <johan.h.kremer@gmail.com>
2025-03-12 09:01:49 +0100

    Forgot that i was in main branch. Add new content to project page.

---
+++ app/projects/[id]/page.tsx
@@ -0,0 +1,56 @@
+import { Separator } from "@radix-ui/themes"
+import Image from "next/image"
+import Link from "next/link"
+import projects from "../../../projects/projects.json"
+
+interface ProjectPageProps {
+    params: {
+        id: string
+    }
+}
+
+export default function ProjectPage({ params }: ProjectPageProps) {
+
+    const projectId = params.id
+    const project = projects.find(project => project.id === projectId)
+
+    if (!project) {
+        return <div>Project not found</div>
+    }
+
+    const { title, description, images, tags, link, githublink } = project
+
+    return (
+        <>
+            <h3>{title}</h3>
+            <div className='py-8'>
+                <Separator orientation="horizontal" size="4" />
+            </div>
+            <div className="flex flexrow">
+                <div className="flex flex-col">
+                    <h5>Description</h5>
+                    <Link href={githublink}>
+                        <p className="text-secondary-200">Project on Github</p>
+                    </Link>
+                    <p>{description}</p>
+                    <h5>Tools used</h5>
+                    <ul>
+                        {tags.map(tag => (
+                            <li key={tag}>{tag}</li>
+                        ))}
+                    </ul>
+                    <button className="button-secondary">
+                        <Link href={link}>
+                            <p>Visit site</p>
+                        </Link>
+                    </button>
+                </div>
+                <div className="flex flex-col">
+                    {images.map(image => (
+                        <Image key={image} src={image} alt={title} width={200} height={200} />
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+}
--- components/server/about-section.tsx
+++ components/server/about-section.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function AboutSection() {
     return (
-        <section id="about" className='min-h-screen p-8'>
+        <section id="contact" className='min-h-screen p-8'>
             <h3>About</h3>
             <div className='py-8'>
                 <Separator orientation="horizontal" size="4" />
