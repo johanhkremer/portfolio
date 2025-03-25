e513b7f77a3e239da3a9f6d3c44a9b34c02d8991
Johan Kremer <johan.h.kremer@gmail.com>
2025-03-25 13:47:08 +0100

    Change styling to arrow-icon

--- components/server/hero-section.tsx
+++ components/server/hero-section.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function HeroSection() {
     return (
-        <section id="hero" className="flex flex-col justify-between h-[calc(100vh-6rem)]">
+        <section id="hero" className="flex flex-col justify-between h-[calc(100dvh-6rem)]">
             <div className="flex flex-col space-y-4 pl-0 pt-14 md:pl-36 md:pt-24">
                 <h1>Johan Kremer</h1>
                 <h2 className='h4'>Front End Developer</h2>
@@ -11,7 +11,7 @@ export default function HeroSection() {
                     <h3 className="h5 text-secondary-200 hover:underline">Projects</h3>
                 </Link>
             </div>
-            <div className="flex justify-center pb-20 md:pb-0">
+            <div className="flex justify-center">
                 <Link href="#projects" aria-label='Scroll down to projects'>
                     <ArrowDown
                         size={48}
