42082243520c4c6c0b1f0e459669bfc7757667b6
Johan Kremer <johan.h.kremer@gmail.com>
2025-03-27 11:41:00 +0100

    Add scroll-mt-20 to hero section so that navbar is visible.

--- components/server/hero-section.tsx
+++ components/server/hero-section.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function HeroSection() {
     return (
-        <section id="hero" className="flex flex-col justify-between h-[calc(100dvh-6rem)]">
+        <section id="hero" className="flex flex-col justify-between h-[calc(100dvh-6rem)] scroll-mt-20">
             <div className="flex flex-col space-y-4 pt-14 md:pt-24 lg:pt-30 sm:gap-6">
                 <h1 className='sm:text-6xls md:text-8xl'>Johan Kremer</h1>
                 <h2 className='h4 sm:text-4xl md:text-5xl'>Front End Developer</h2>
