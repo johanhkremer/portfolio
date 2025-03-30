f67741de592d48c4fcb289ba5b6df1bc0d83217c
Johan Kremer <johan.h.kremer@gmail.com>
2025-03-30 18:37:11 +0200

    Refactord breakpoints for hero text

--- components/server/hero-section.tsx
+++ components/server/hero-section.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link';
 export default function HeroSection() {
     return (
         <section id="hero" className="flex flex-col justify-between h-[calc(100dvh-6rem)] scroll-mt-20">
-            <div className="flex flex-col space-y-4 pt-14 md:pt-24 lg:pt-30 sm:gap-6">
-                <h1 className='sm:text-6xls md:text-8xl'>Johan Kremer</h1>
-                <h2 className='h4 sm:text-4xl md:text-5xl'>Front End Developer</h2>
+            <div className="flex flex-col space-y-4 pt-14 md:pt-24 2xl:pt-52 sm:gap-6">
+                <h1 className='sm:text-6xls md:text-8xl 2xl:text-9xl'>Johan Kremer</h1>
+                <h2 className='h4 sm:text-4xl md:text-5xl 2xl:text-6xl'>Front End Developer</h2>
                 <Link href="#projects">
-                    <h3 className="h5 sm:text-3xl md:text-4xl text-secondary-200 hover:underline">Projects</h3>
+                    <h3 className="h5 sm:text-3xl md:text-4xl 2xl:text-5xl text-secondary-200 hover:underline">Projects</h3>
                 </Link>
             </div >
             <div className="flex justify-center">
